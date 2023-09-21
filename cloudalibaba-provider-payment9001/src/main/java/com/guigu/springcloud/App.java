5df59484cb83dc67ee59d3a66a02ff99f034a4ee
hege-der <zzy15738567781@163.com>
2023-09-21 16:22:42 +0800

    Initial commit Create config-test.yml

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/guigu/springcloud/App.java
@@ -0,0 +1,13 @@
+package com.guigu.springcloud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
